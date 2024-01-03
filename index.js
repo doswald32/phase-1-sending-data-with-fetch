a93127e77b9956ee9ed7358fc5c5b943e785d34e
doswald32 <dan.oswald32@gmail.com>
2024-01-02 23:10:03 -0700

    stuck. just using this as a base so I don't start from scratch

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+let newData = document.createElement("li");
+
+function submitData() {
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((data) => addData(data))
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }, 
+    body: JSON.stringify({
+        name: "Steve",
+        email: "steve@steve.com",
+    })
+};
+
+/*function addData(data) {
+    const newElement = document.createElement("li");
+    newElement.textContent = data;
+    document.querySelector("form").appendChild(newElement);
+    console.log(newElement);
+}*/
+
+submitData();
+
+
